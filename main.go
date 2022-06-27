228102d75aab48a31b310466963636a3b6babe58
Sam Brougham <sam.brougham@gmail.com>
2022-06-27 18:53:32 -0400

    Set up image upload and processing

--- main.go
+++ main.go
@@ -1,13 +1,62 @@
 package main
 
-import "syscall/js"
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"image"
+	"log"
+	"syscall/js"
+
+	"image/color"
+	"image/draw"
+	"image/jpeg"
+	_ "image/png"
+)
+
+func setImgSrc(img image.Image) {
+	doc := js.Global().Get("document")
+	imageEl := doc.Call("getElementById", "output")
+
+	var buff bytes.Buffer
+	jpeg.Encode(&buff, img, nil)
+
+	imageEl.Set("src", fmt.Sprintf("data:image/jpeg;base64,%s", base64.StdEncoding.EncodeToString(buff.Bytes())))
+}
+
+func analyze(this js.Value, inputs []js.Value) interface{} {
+	imageArr := inputs[0]
+	srcBytes := make([]uint8, imageArr.Get("byteLength").Int())
+	js.CopyBytesToGo(srcBytes, imageArr)
+
+	reader := bytes.NewReader(srcBytes)
+	img, _, err := image.Decode(reader)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	bounds := img.Bounds()
+
+	rgba, ok := img.(*image.RGBA)
+	if !ok {
+		b := img.Bounds()
+		rgba = image.NewRGBA(image.Rect(0, 0, b.Dx(), b.Dy()))
+		draw.Draw(rgba, rgba.Bounds(), img, b.Min, draw.Src)
+	}
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			// r, g, b, a := img.At(x, y).RGBA()
+			rgba.Set(x, y, color.RGBA{85, 165, 34, 255})
+		}
+	}
+
+	setImgSrc(rgba)
+
+	return imageArr
+}
 
 func main() {
-	doc := js.Global().Get("document")
-	body := doc.Get("body")
-
-	h1 := doc.Call("createElement", "h1")
-	h1.Set("innerHTML", "Working!")
-
-	body.Call("append", h1)
+	js.Global().Set("analyze", js.FuncOf(analyze))
+	select {}
 }
