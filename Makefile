d14fd9ff09ef59fb03f69c49a0084f6dca267b1b
Sam Brougham <sam.brougham@gmail.com>
2022-06-29 12:27:27 -0400

    Add download button and mobile styling

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 web:
 	mkdir -p build
 	cp $(shell go env GOROOT)/misc/wasm/wasm_exec.js build/
-	cp web/* build/
+	cp -r web/* build/
 	GOOS=js GOARCH=wasm go build -o build/main.wasm
--- web/seam.js
+++ web/seam.js
@@ -1,3 +1,59 @@
+const widthRangeEl = document.getElementById("width-range");
+const heightRangeEl = document.getElementById("height-range");
+const imageEl = document.getElementById("output");
+const overlayEl = document.getElementById("overlay");
+
+const onBufferLoad = (buffer) => {
+  const arrayBuffer = new Uint8Array(buffer.target.result);
+  analyze(arrayBuffer);
+};
+
+const readFile = (event) => {
+  const file = event.target.files[0];
+  if (!file) return;
+
+  const reader = new FileReader();
+  reader.onload = onBufferLoad;
+  reader.readAsArrayBuffer(file);
+};
+
+const setRanges = () => {
+  widthRangeEl.value = 50;
+  heightRangeEl.value = 50;
+  widthRangeEl.max = Math.round(imageEl.naturalWidth) - 100;
+  heightRangeEl.max = Math.round(imageEl.naturalHeight) - 100;
+  updateOverlay();
+};
+
+const updateOverlay = () => {
+  const widthMax = imageEl.naturalWidth;
+  const widthValue = widthRangeEl.value;
+  const widthPercent = ((widthValue / widthMax) * 100) / 2;
+
+  const heightMax = imageEl.naturalHeight;
+  const heightValue = heightRangeEl.value;
+  const heightPercent = ((heightValue / heightMax) * 100) / 2;
+
+  const background = `
+    linear-gradient(90deg, black ${widthPercent}%, transparent ${widthPercent}%),
+    linear-gradient(-90deg, black ${widthPercent}%, transparent ${widthPercent}%),
+    linear-gradient(0deg, black ${heightPercent}%, transparent ${heightPercent}%),
+    linear-gradient(180deg, black ${heightPercent}%, transparent ${heightPercent}%)
+  `;
+  overlayEl.style.background = background;
+};
+
+const onClickCarve = () => {
+  // CARVE
+};
+
+const onClickDownload = () => {
+  const link = document.createElement("a");
+  link.setAttribute("href", imageEl.src);
+  link.setAttribute("download", "resized.jpg");
+  link.click();
+};
+
 const initialize = () =>
   fetch("./dali.jpeg")
     .then((r) => r.arrayBuffer())
@@ -17,60 +73,3 @@ const loadAndInitWA = (waURL) => {
 };
 
 loadAndInitWA("main.wasm");
-
-const widthRangeEl = document.getElementById("width-range");
-const heightRangeEl = document.getElementById("height-range");
-const imageEl = document.getElementById("output");
-
-const onBufferLoad = (buffer) => {
-  const arrayBuffer = new Uint8Array(buffer.target.result);
-  analyze(arrayBuffer);
-};
-
-const readFileFromEvent = (event) => {
-  const file = (event.dataTransfer || event.target).files[0];
-  if (!file) return;
-
-  const reader = new FileReader();
-  reader.onload = onBufferLoad;
-  reader.readAsArrayBuffer(file);
-};
-
-const cancelEvent = (event) => {
-  event.stopPropagation();
-  event.preventDefault();
-};
-
-const setRanges = () => {
-  widthRangeEl.value = 0;
-  heightRangeEl.value = 0;
-  widthRangeEl.max = Math.round(imageEl.naturalWidth) - 100;
-  heightRangeEl.max = Math.round(imageEl.naturalHeight) - 100;
-};
-
-const updateRange = () => {
-  const widthMax = imageEl.naturalWidth;
-  const widthValue = widthRangeEl.value;
-  const widthPercent = ((widthValue / widthMax) * 100) / 2;
-
-  const heightMax = imageEl.naturalHeight;
-  const heightValue = heightRangeEl.value;
-  const heightPercent = ((heightValue / heightMax) * 100) / 2;
-
-  setOverlay(widthPercent, heightPercent);
-};
-
-const setOverlay = (width, height) => {
-  const background = `
-    linear-gradient(90deg, black ${width}%, transparent ${width}%),
-    linear-gradient(-90deg, black ${width}%, transparent ${width}%),
-    linear-gradient(0deg, black ${height}%, transparent ${height}%),
-    linear-gradient(180deg, black ${height}%, transparent ${height}%)
-  `;
-  document.getElementById("overlay").style.background = background;
-};
-
-const onDropFile = (event) => {
-  cancelEvent(event);
-  readFileFromEvent(event);
-};
