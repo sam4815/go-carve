f28713fb9409414021986d3e11d99068d8f0bfe7
sam4815 <32017929+sam4815@users.noreply.github.com>
2023-05-03 12:33:06 +0000

    Deploying to gh-pages from @ sam4815/go-carve@442b338e5f4e52d1733b6d7d136bf754dfeffb93 🚀

--- seam.js
+++ seam.js
@@ -76,7 +76,7 @@ const onClickDownload = () => {
 };
 
 const initialize = () =>
-  fetch("https://random.imagecdn.app/650/450")
+  fetch("https://picsum.photos/650/450")
     .then((r) => r.arrayBuffer())
     .then((buffer) => {
       const arrayBuffer = new Uint8Array(buffer);
